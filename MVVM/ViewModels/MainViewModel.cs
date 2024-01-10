503fe948138f44c8bec50b4636eeca8070e6d94e
TA-800 <taherali0905@gmail.com>
2024-01-10 11:15:11 +0300

    refactor: cleaned files

--- MVVM/ViewModels/MainViewModel.cs
+++ MVVM/ViewModels/MainViewModel.cs
@@ -9,8 +9,6 @@ namespace Elementary.MVVM.ViewModels
 
     public class MainViewModel : ObservableObject
     {
-
-        UserStore userStore;
         HomeViewModel homeViewModel;
 
         public ObservableCollection<PageId> NavigationItemList { get; set; }
@@ -21,27 +19,12 @@ namespace Elementary.MVVM.ViewModels
             set
             {
                 currentPage = value; OnPropertyChanged();
-                
-                switch(currentPage)
-                {
-                    case PageId.Discover:
-
-                        break;
-                    case PageId.Guide:
-                        break;
-                    case PageId.Information:
-                        break;
-                    default:
-                        break;
-                }
             }
         }
         public MainViewModel()
         {
             // Default page
             CurrentPage = PageId.Discover;
-
-            // Data Contexts
 
             // Navigation items = PageId.Home, About, Info
             NavigationItemList = new ObservableCollection<PageId>
